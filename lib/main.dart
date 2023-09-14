44703b0541caf1514f3b3984574fe898e3249631
ChoiSungYung <kenshin753@gmail.com>
2023-09-14 14:30:14 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WebView Sample',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WebView Sample'),
+      ),
+      body: const WebView(
+        initialUrl: 'http://sample.dosirock.kr',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
